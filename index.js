5c1d1edcab54312a71ac884a0961837c12779bf3
SimonGideon <simongideon918@gmail.com>
2022-10-28 14:13:30 +0300

    Refactor the whole code

--- index.js
+++ index.js
@@ -1,67 +1,121 @@
-/* eslint-disable no-plusplus */
-const addButton = document.getElementById('addme');
+const addButton = document.getElementById('addButton');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-const addBook = document.getElementById('myShelve');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
+const list = document.getElementById('list'); // link LIST
+const addBook = document.getElementById('addBook');
 
-// maintain the list on refresh page
+const addNew = document.getElementById('addNew'); // link ADDNEW
+const newBook = document.getElementById('newBook');
+
+const contact = document.getElementById('contact'); // link CONTACT
+const contactBook = document.getElementById('contactBook');
+
+/* initialize page */
+newBook.style.display = 'block';
+contactBook.style.display = 'none';
+addBook.style.display = 'none';
+
+/* navbar links */
+list.addEventListener('click', () => {
+  newBook.style.display = 'none';
+  contactBook.style.display = 'none';
+  addBook.style.display = 'block';
+  list.style.color = 'blue';
+  addNew.style.color = 'black';
+  contact.style.color = 'black';
+});
+
+addNew.addEventListener('click', () => {
+  newBook.style.display = 'block';
+  addBook.style.display = 'none';
+  contactBook.style.display = 'none';
+  list.style.color = 'black';
+  addNew.style.color = 'blue';
+  contact.style.color = 'black';
+});
+
+contact.addEventListener('click', () => {
+  newBook.style.display = 'none';
+  addBook.style.display = 'none';
+  contactBook.style.display = 'block';
+  list.style.color = 'black';
+  addNew.style.color = 'black';
+  contact.style.color = 'blue';
+});
+/* ****** */
+
+/* addBook - hold books after every refresh page */
 if (localStorage.getItem('books') !== null) {
   const getbook = JSON.parse(localStorage.getItem('books'));
+
   getbook.forEach((item) => {
     addBook.innerHTML += `
-      <div>
-        <p>${item.title}</p>
-        <p>${item.author}</p>
+      <div class="books">
+        <p>"${item.title}" by "${item.author}"</p>
         <button class="remove" name="${item.title}">Remove</button>
-        <hr>
       </div>
     `;
   });
+}
+/* ***** */
+
+class Methods {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  addbook = () => {
+    addBook.innerHTML += `
+    <div class="books">
+      <p>"${title.value}" by "${author.value}"</p>
+      <button class="remove" name="${title.value}">Remove</button>
+    </div>
+  `;
+    const book1 = { title: this.title, author: this.author };
+    if (localStorage.getItem('books') === null) {
+      const books = [];
+      books.push(book1);
+      localStorage.setItem('books', JSON.stringify(books));
+    } else {
+      const books = JSON.parse(localStorage.getItem('books'));
+      books.push(book1);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+  removebook = () => {
+    /* remove button */
+    const remove = document.querySelectorAll('.remove');
+
+    remove.forEach((item) => {
+      item.addEventListener('click', () => {
+        item.parentElement.remove();
+        const bookname = item.name;
+
+        /* remove from localStorage */
+        const getremove = JSON.parse(localStorage.getItem('books'));
+
+        const newArr = getremove.filter((object) => object.title !== bookname);
+
+        /* update localstorage */
+        localStorage.setItem('books', JSON.stringify(newArr));
+      });
+    });
+  }
 }
 
 addButton.addEventListener('click', (e) => {
   e.preventDefault();
 
-  addBook.innerHTML += `
-    <div>
-      <p>${title.value}</p>
-      <p>${author.value}</p>
-      <button class="remove" name="${title.value}">Remove</button>
-      <hr>
-    </div>
-  `;
+  /* add book with method class */
+  const ui = new Methods(title.value, author.value);
+  ui.addbook();
 
-  const book1 = new Book(title.value, author.value);
+  /* remove book with method class */
+  ui.removebook();
 
-  if (localStorage.getItem('books') === null) {
-    const books = [];
-    books.push(book1);
-    localStorage.setItem('books', JSON.stringify(books));
-  } else {
-    const books = JSON.parse(localStorage.getItem('books'));
-    books.push(book1);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-  // clear the form after inputs
   title.value = '';
   author.value = '';
-});
-
-// remove btn
-const remove = document.querySelectorAll('.remove');
-remove.forEach((item) => {
-  item.addEventListener('click', () => {
-    item.parentElement.remove();
-    const bookname = item.name;
-    // remove the object from the local storage
-    const getremove = JSON.parse(localStorage.getItem('books'));
-    const newArr = getremove.filter((object) => object.title !== bookname);
-    // update the local storage with the new object array
-    localStorage.setItem('books', JSON.stringify(newArr));
-  });
 });
